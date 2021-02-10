b953eec4d8f80b23c0d0dc61a1d85a6f92eab0ca
Guido Kleine <guidokleine1991@gmail.com>
2021-02-10 20:39:00 +0100

    Setting up webpack

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
+# Flix-tv
+
 ## Description
 A simple overview app of tv shows, using the open source tv maze api -> https://www.tvmaze.com/api
+
+## Installation
+
+Use the package manager [npm](https://docs.npmjs.com/getting-started) to install flix-tv.
+
+```bash
+npm install foobar
+```
+
+## Scripts
+
+npm run start:
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+const path = require("path");
 
-app.get('/', (req, res) => {
-  res.send(' Hello World');
+const indexHTML = (() => {
+  return fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf-8");
+});
+
+app.use("/dist", express.static(path.resolve(__dirname, "./dist")));
+
+app.get('*', (req, res) => {
+  res.send(indexHTML);
 });
 
 const port = process.env.PORT || 8080;
